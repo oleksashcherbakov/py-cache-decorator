72921e4397bd7245e36ed05f2ee4c585c9a068be
Alex <oleksa.shcherbakov@gmail.com>
2024-07-31 15:21:39 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,12 +5,14 @@ def cache(func: Callable) -> Callable:
     collected_data = {}
 
     def inner(*args, **kwargs) -> int:
-        if args in collected_data:
+        elem = (args, func.__name__)
+
+        if elem in collected_data:
             print("Getting from cache")
-            return collected_data[args]
+            return collected_data[elem]
         else:
             print("Calculating new result")
             result = func(*args, **kwargs)
-            collected_data[args] = result
+            collected_data[elem] = result
             return result
     return inner
